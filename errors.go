9655829517729249ff01b8c2f654418c887fc642
James Sapara <james.sapara@gmail.com>
2015-10-30 09:45:44 -0700

    copied errors from dbr, helping avoid an import of dbr to test errors

---
+++ errors.go
@@ -0,0 +1,13 @@
+package norm
+
+import (
+  "github.com/gocraft/dbr"
+)
+
+// Copy errors from dbr
+var ErrArgumentMismatch := dbr.ErrArgumentMismatch
+var ErrInvalidSliceLength := dbr.ErrInvalidSliceLength
+var ErrInvalidSliceValue := dbr.ErrInvalidSliceValue
+var ErrInvalidValue := dbr.ErrInvalidValue
+var ErrNotFound := dbr.ErrNotFound
+var ErrNotUTF8 := dbr.ErrNotUTF8
