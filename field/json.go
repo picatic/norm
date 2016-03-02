9d0df3c5b103909c57f444dd1a121901cadd97ea
Adam Brykajlo <adam.brykajlo@gmail.com>
2016-03-02 15:07:59 -0800

    add json.go

---
+++ field/json.go
@@ -0,0 +1,85 @@
+package field
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+)
+
+// JSON field type, does not allow nil
+type JSON struct {
+	JSON   interface{}
+	shadow interface{}
+	ShadowInit
+}
+
+// Scan a value into the string, error on nil
+func (j *JSON) Scan(value interface{}) error {
+	switch value := value.(type) {
+	case nil:
+		j.JSON = nil
+		return nil
+	case string:
+		return j.UnmarshalJSON([]byte(value))
+	case []byte:
+		return j.UnmarshalJSON(value)
+	case map[string]interface{}:
+		_, err := json.Marshal(value)
+		if err != nil {
+			return err
+		}
+		j.JSON = value
+		return nil
+	case []interface{}:
+		_, err := json.Marshal(value)
+		if err != nil {
+			return err
+		}
+		j.JSON = value
+		return nil
+	default:
+		return errors.New("Unrecognized type")
+	}
+}
+
+// Value return the value of this field
+func (j JSON) Value() (driver.Value, error) {
+	if j.JSON == nil {
+		return nil, nil
+	}
+
+	return j.MarshalJSON()
+}
+
+// ShadowValue return the initial value of this field
+func (j JSON) ShadowValue() (driver.Value, error) {
+	if j.InitDone() {
+		if j.shadow == nil {
+			return nil, nil
+		}
+
+		return json.Marshal(j.shadow)
+	}
+
+	return nil, ErrorUnintializedShadow
+}
+
+// IsDirty if the shadow value does not match the field value
+func (j *JSON) IsDirty() bool {
+	return j.shadow != j.JSON
+}
+
+//IsSet indicates if Scan has been called successfully
+func (j JSON) IsSet() bool {
+	return j.InitDone()
+}
+
+// MarshalJSON Marshal just the value of JSON
+func (j JSON) MarshalJSON() ([]byte, error) {
+	return json.Marshal(j.JSON)
+}
+
+// UnmarshalJSON implements encoding/json Unmarshaler
+func (j *JSON) UnmarshalJSON(data []byte) error {
+	return json.Unmarshal(data, &j.JSON)
+}
