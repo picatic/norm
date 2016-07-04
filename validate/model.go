73faf923ed087da33c4ecd8db00414d6b68ace2e
Adam Brykajlo <adam.brykajlo@gmail.com>
2016-07-04 15:39:58 -0700

    change modelvalidator to mapvalidator to change function purpose

--- validate/model.go
+++ validate/model.go
@@ -4,12 +4,14 @@ import (
 	"github.com/picatic/norm/field"
 )
 
-type ModelValidator map[field.Name]Validator
+//MapValidator provides a way to build validators from a select list of validators
+//kind of like All except you can select which fields get validated
+type MapValidator map[field.Name]Validator
 
-func (mv ModelValidator) Fields(fields field.Names) Validator {
+func (mv MapValidator) Fields(fields field.Names) Validator {
 	return ValidatorFunc(func(model interface{}) error {
 		for _, fieldName := range fields {
-			err := Field(fieldName, mv[fieldName]).Validate(model)
+			err := mv[fieldName].Validate(model)
 
 			if err != nil {
 				return err
