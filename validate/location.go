1520233183a947d701d83a1319e627e6d683f3a8
Adam Brykajlo <adam.brykajlo@gmail.com>
2016-07-06 14:43:04 -0700

    add InList builder and proper validation error bubbling with aliases

--- validate/location.go
+++ validate/location.go
@@ -13,3 +13,19 @@ type Index int
 func (i Index) Location() string {
 	return fmt.Sprintf("[%d]", int(i))
 }
+
+type EmbeddedLocation []Locationer
+
+func (el EmbeddedLocation) Location() string {
+	result := el[0].Location()
+	for i := 1; i < len(el); i++ {
+		if l, ok := el[i].(Index); ok {
+			result += l.Location()
+			continue
+		}
+
+		result += "." + el[i].Location()
+	}
+
+	return result
+}
--- validate/validation.go
+++ validate/validation.go
@@ -24,42 +24,8 @@ var Never ValidatorFunc = func(_ interface{}) error {
 	return errors.New("NEVER!!!!")
 }
 
-var UUID ValidatorFunc = func(v interface{}) error {
-	str, ok := v.(string)
-	if !ok {
-		return ErrNotString
-	}
+var UUID = String("UUID", govalidator.IsUUID)
 
-	valid := govalidator.IsUUID(str)
-	if !valid {
-		return errors.New("the string \"" + str + "\" is not an UUID")
-	}
+var Email = String("email", govalidator.IsEmail)
 
-	return nil
-}
-
-var Email ValidatorFunc = func(v interface{}) error {
-	str, ok := v.(string)
-	if !ok {
-		return ErrNotString
-	}
-
-	valid := govalidator.IsEmail(str)
-	if !valid {
-		return errors.New("the string \"" + str + "\" is not an email")
-	}
-
-	return nil
-}
-
-var URL ValidatorFunc = func(v interface{}) error {
-	str, ok := v.(string)
-	if !ok {
-		return ErrNotString
-	}
-
-	valid := govalidator.IsURL(str)
-	if !valid {
-		return errors.New("the string \"" + str + "\" is not an URL")
-	}
-}
+var URL = String("URL", govalidator.IsURL)
