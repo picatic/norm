49508b8e4e2bdbb75574406cfed71c66b86474d0
Kevin Pierce <kevpie@gmail.com>
2016-12-09 09:46:29 -0800

    Remove mutex from shadow. - if thread safety around a norm model is required use locking external to the norm model.

--- field/shadow.go
+++ field/shadow.go
@@ -1,31 +1,19 @@
 package field
 
-import (
-	"sync"
-	"sync/atomic"
-)
-
 // ShadowInit Making a shadowValue only settable once
 type ShadowInit struct {
-	m    sync.Mutex
-	done uint32
+	done bool
 }
 
 // DoInit set a value by func
 func (o *ShadowInit) DoInit(f func()) {
-	if atomic.LoadUint32(&o.done) == 1 {
-		return
-	}
-	// Slow-path.
-	o.m.Lock()
-	defer o.m.Unlock()
-	if o.done == 0 {
-		defer atomic.StoreUint32(&o.done, 1)
+	if o.done == false {
 		f()
+		o.done = true
 	}
 }
 
 // InitDone return a value
 func (o *ShadowInit) InitDone() bool {
-	return atomic.LoadUint32(&o.done) == 1
+	return o.done
 }
